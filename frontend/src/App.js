cc05a077f5158ae256dbeb3cc611a6ce5b6093e6
yildiz-harun <yildizharun@outlook.com>
2022-09-10 21:13:21 +0300

    Feat: auth context, routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { useContext } from "react";
+import Home from "./pages/Home"
+import Signup from "./pages/Signup"
+import Login from "./pages/Login"
+import { AuthContext } from "./contexts/AuthContextProvider"
+import Navbar from "./components/Navbar";
 
 function App() {
+  const { user, setUser } = useContext(AuthContext)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Navigate to="login" />} />
+        <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
+        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
