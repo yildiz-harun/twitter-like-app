4bec6ce11b8ac9b5bc37d113e39a39da9e281d69
yildiz-harun <yildizharun@outlook.com>
2022-09-10 21:10:06 +0300

    Feat: AuthContextProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AuthContextProvider from './contexts/AuthContextProvider';
 import PostsContextProvider from './contexts/PostsContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PostsContextProvider>
-      <App />
-    </PostsContextProvider>
+    <AuthContextProvider>
+      <PostsContextProvider>
+        <App />
+      </PostsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
