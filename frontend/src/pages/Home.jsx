35a5f3a2e3415e27bc5c174c8b12157813b44e9a
yildiz-harun <yildizharun@outlook.com>
2022-09-10 21:11:18 +0300

    Add: home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Form from '../components/Form'
+import PostsList from '../components/PostsList'
+
+function Home() {
+  return (
+    <div>
+      <Form />
+      <PostsList />
+    </div>
+  )
+}
+
+export default Home
