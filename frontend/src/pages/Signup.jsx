7efb1366dfe330648d389df17bc6617fde8bfafd
yildiz-harun <yildizharun@outlook.com>
2022-09-11 18:22:57 +0300

    Feat: signup functionality

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,14 +1,43 @@
+import { useState, useContext } from 'react'
+import axios from 'axios'
+import { AuthContext } from '../contexts/AuthContextProvider'
+
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 function Signup() {
+  const { user, setUser } =
+    useContext(AuthContext)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] =
+    useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    const response = await axios.post(
+      'http://localhost:5000/api/user/signup',
+      { email: email, password: password }
+    )
+
+    // save the user to local storage
+    localStorage.setItem(
+      'user',
+      JSON.stringify(response)
+    )
+    setUser(response)
+    console.log(response)
+    setIsLoading(false)
+  }
+
   return (
     <Container>
       <Row>
         <Col md={{ span: 6, offset: 3 }}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group
               className="mb-3"
               controlId="formBasicEmail"
@@ -19,6 +48,10 @@ function Signup() {
               <Form.Control
                 type="email"
                 placeholder="Enter email"
+                onChange={(e) =>
+                  setEmail(e.target.value)
+                }
+                value={email}
               />
             </Form.Group>
 
@@ -30,6 +63,10 @@ function Signup() {
               <Form.Control
                 type="password"
                 placeholder="Password"
+                onChange={(e) =>
+                  setPassword(e.target.value)
+                }
+                value={password}
               />
             </Form.Group>
             <Form.Group
@@ -39,6 +76,7 @@ function Signup() {
             <Button
               variant="primary"
               type="submit"
+              disabled={isLoading}
             >
               Sign up
             </Button>
