4aa6d1771900f87e5ad8fda178a60c1f7c96ed82
yildiz-harun <yildizharun@outlook.com>
2022-09-11 11:58:45 +0300

    Refactor: user's inital state is now null

--- frontend/src/contexts/AuthContextProvider.js
+++ frontend/src/contexts/AuthContextProvider.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react"
 export const AuthContext = createContext()
 
 function AuthContextProvider({ children }) {
-    const [user, setUser] = useState()
+    const [user, setUser] = useState(null)
     return (
         < AuthContext.Provider value={{ user, setUser }} >{children}</AuthContext.Provider >
     )
