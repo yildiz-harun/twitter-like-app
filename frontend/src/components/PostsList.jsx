a2b6ce32a1f19427275514549847d436c8c899ee
yildiz-harun <yildizharun@outlook.com>
2022-09-11 18:20:40 +0300

    Update: post form component

--- frontend/src/components/PostsList.jsx
+++ frontend/src/components/PostsList.jsx
@@ -1,5 +1,44 @@
+import { useContext, useEffect } from 'react'
+import axios from 'axios'
+import { AuthContext } from '../contexts/AuthContextProvider'
+import { PostsContext } from '../contexts/PostsContextProvider'
+import Post from './Post'
+
 function PostsList() {
-  return <div>PostsList</div>
+  const { posts, setPosts } =
+    useContext(PostsContext)
+  const { user, setUser } =
+    useContext(AuthContext)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        var authToken =
+          'Bearer ' + user.data.token
+        //const config = { headers: { "Authorization": `Bearer ${user.data.token}` } }
+
+        const response = await axios.get(
+          'http://localhost:5000/api/posts',
+          {
+            headers: { Authorization: authToken },
+          }
+        )
+
+        setPosts(response.data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [user, setUser])
+
+  return (
+    <div>
+      {posts.map((post, index) => (
+        <Post key={index} post={post} />
+      ))}
+    </div>
+  )
 }
 
 export default PostsList
