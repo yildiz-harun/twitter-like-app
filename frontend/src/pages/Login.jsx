86e553d9fbe71394fdd94dc75f647fc4cab482fe
yildiz-harun <yildizharun@outlook.com>
2022-09-11 11:55:56 +0300

    Update: initial login page with react-bootstrap

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,54 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <Container>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="formBasicEmail"
+              >
+                <Form.Label>
+                  Email address
+                </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                />
+              </Form.Group>
+
+              <Form.Group
+                className="mb-3"
+                controlId="formBasicPassword"
+              >
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="formBasicCheckbox"
+              ></Form.Group>
+              <Button
+                variant="primary"
+                type="submit"
+              >
+                Log in
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
