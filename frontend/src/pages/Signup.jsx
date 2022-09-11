cbb324bc54a9fc1030b8674dd310a6811fbf7054
yildiz-harun <yildizharun@outlook.com>
2022-09-11 11:56:56 +0300

    Update: initial signup page with react-bootstrap

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,51 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 function Signup() {
   return (
-    <div>Signup</div>
+    <Container>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicEmail"
+            >
+              <Form.Label>
+                Email address
+              </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+              />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicPassword"
+            >
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicCheckbox"
+            ></Form.Group>
+            <Button
+              variant="primary"
+              type="submit"
+            >
+              Sign up
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
